104211ebb87e495592cd3de01a29ddbbd7f15089
Kevin Berry <xckjb88@gmail.com>
2011-04-23 19:33:30 -0400

    2 char spacing

--- echoserver-time.c
+++ echoserver-time.c
@@ -13,51 +13,51 @@
 
 int main(int argc, char **argv) {
 
-    struct addrinfo hints, *res;
-    struct sockaddr_storage their_addr;
-    socklen_t addr_size;
-    char *listenport;
-    int listenfd, connfd;
-    char buf[1024];
-    size_t bytes;
-    int yes;
+  struct addrinfo hints, *res;
+  struct sockaddr_storage their_addr;
+  socklen_t addr_size;
+  char *listenport;
+  int listenfd, connfd;
+  char buf[1024];
+  size_t bytes;
+  int yes;
 
-    listenport = argv[1];
+  listenport = argv[1];
 
-    bzero(&hints, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+  bzero(&hints, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
 
-    getaddrinfo(NULL, listenport, &hints, &res);
-    listenfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    bind(listenfd, res->ai_addr, res->ai_addrlen);
-    listen(listenfd, BACKLOG);
+  getaddrinfo(NULL, listenport, &hints, &res);
+  listenfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+  bind(listenfd, res->ai_addr, res->ai_addrlen);
+  listen(listenfd, BACKLOG);
 
 
-    printf("Waiting for connections...\n");
-    while (1) {
-        addr_size = sizeof(their_addr);
-        connfd = accept(listenfd, (struct sockaddr*)&their_addr, &addr_size);
-        yes = 60;
-        setsockopt(connfd, SOL_SOCKET, SO_RCVTIMEO, &yes, sizeof(int));
-        printf("got connection.\n");
+  printf("Waiting for connections...\n");
+  while (1) {
+      addr_size = sizeof(their_addr);
+      connfd = accept(listenfd, (struct sockaddr*)&their_addr, &addr_size);
+      yes = 60;
+      setsockopt(connfd, SOL_SOCKET, SO_RCVTIMEO, &yes, sizeof(int));
+      printf("got connection.\n");
 
-        if (!fork())  {
-            close(listenfd);
-            while ((bytes = recv(connfd, buf, sizeof(buf), 0)))  {
-                sleep(2);
-                send(connfd, buf, strlen(buf), 0);
-            }
-            close(connfd);
-            exit(0);
-        }
-        close(connfd);
-    }
+      if (!fork())  {
+          close(listenfd);
+          while ((bytes = recv(connfd, buf, sizeof(buf), 0)))  {
+              sleep(2);
+              send(connfd, buf, strlen(buf), 0);
+          }
+          close(connfd);
+          exit(0);
+      }
+      close(connfd);
+  }
 
-    freeaddrinfo(res);
-    close(listenfd);
-    close(connfd);
+  freeaddrinfo(res);
+  close(listenfd);
+  close(connfd);
 
-    return 0;
+  return 0;
 }
--- echoserver.c
+++ echoserver.c
@@ -12,37 +12,37 @@
 
 int main(int argc, char **argv) {
 
-    struct addrinfo hints, *res;
-    struct sockaddr_storage their_addr;
-    socklen_t addr_size;
-    char *listenport;
-    int listenfd, connfd, yes;
-    char buf[1024];
-    size_t bytes;
+  struct addrinfo hints, *res;
+  struct sockaddr_storage their_addr;
+  socklen_t addr_size;
+  char *listenport;
+  int listenfd, connfd, yes;
+  char buf[1024];
+  size_t bytes;
 
-    listenport = argv[1];
+  listenport = argv[1];
 
-    bzero(&hints, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+  bzero(&hints, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
 
-    int err = getaddrinfo(NULL, listenport, &hints, &res);
-    printf("%d %s\n", err, gai_strerror(errno));
-    listenfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    bind(listenfd, res->ai_addr, res->ai_addrlen);
-    listen(listenfd, BACKLOG);
-    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+  int err = getaddrinfo(NULL, listenport, &hints, &res);
+  printf("%d %s\n", err, gai_strerror(errno));
+  listenfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+  bind(listenfd, res->ai_addr, res->ai_addrlen);
+  listen(listenfd, BACKLOG);
+  setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
-    addr_size = sizeof(their_addr);
-    connfd = accept(listenfd, (struct sockaddr*)&their_addr, &addr_size);
-    while ((bytes = recv(connfd, buf, sizeof(buf), 0)))  {
-        send(connfd, buf, strlen(buf), 0);
-    }
+  addr_size = sizeof(their_addr);
+  connfd = accept(listenfd, (struct sockaddr*)&their_addr, &addr_size);
+  while ((bytes = recv(connfd, buf, sizeof(buf), 0)))  {
+      send(connfd, buf, strlen(buf), 0);
+  }
 
-    freeaddrinfo(res);
-    close(listenfd);
-    close(connfd);
+  freeaddrinfo(res);
+  close(listenfd);
+  close(connfd);
 
-    return 0;
+  return 0;
 }
