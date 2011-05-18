d2cb8781b9de14ec3b428d9d2ead826cdb1a6bb0
Kevin Berry <xckjb88@gmail.com>
2011-05-18 12:45:58 -0400

    Use more descriptive variable name.

--- sendall.c
+++ sendall.c
@@ -4,15 +4,15 @@
 
 int sendall(int sfd, char *buf, int len, int flags)  {
 
-  int sent = 0, left = len, n = len;
+  int sent = 0, bytesleft = len, n = len;
 
   while (sent < n)  {
-    left = send(sfd, buf + sent, len, flags);
-    if (left == -1) {
+    bytesleft = send(sfd, buf + sent, len, flags);
+    if (bytesleft == -1) {
       return -1;
     }
-    sent += left;
-    len -= left;
+    sent += bytesleft;
+    len -= bytesleft;
   }
   return 0;
 }
