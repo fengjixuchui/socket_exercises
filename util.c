fc16b0879aaa86f59c84259ff4dc7859f32cbaa1
Kevin Berry <xckjb88@gmail.com>
2011-05-21 18:00:49 -0400

    Consolidated socket, bind, listen, setnonblock, die routines to util.

--- util.c
+++ util.c
@@ -1,9 +1,12 @@
 #include "util.h"
 
+void die(const char *strerr)  {
+  fprintf(stderr, "%s\n", strerr);
+  exit(errno);
+}
+
 int sendall(int sfd, char *buf, int len, int flags)  {
-
   int sent = 0, bytesleft = len, n = len;
-
   while (sent < n)  {
     bytesleft = send(sfd, buf + sent, len, flags);
     if (bytesleft == -1) {
@@ -16,12 +19,52 @@ int sendall(int sfd, char *buf, int len, int flags)  {
 }
 
 void hangup(int *sfd) {
-
   printf("Connection closed on socket %d\n", *sfd);
-
   /* Close the socket. */
   close(*sfd);
-
   /* "Remove" the socket from the poll array. */
   *sfd = -1;
+}
+
+void setnonblock(int fd) {
+  int sflags;
+  sflags = fcntl(fd, F_GETFL);
+  sflags |= O_NONBLOCK;
+  fcntl(fd, F_SETFL, sflags);
+}
+
+int getlistenfd(char *port) {
+
+  int lfd;
+  struct addrinfo hints, *res;
+  struct sockaddr_storage their_addr;
+  socklen_t addr_size;
+  int yes, err;
+
+  bzero(&hints, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  if ((err = getaddrinfo(NULL, port, &hints, &res)))  {
+    die(gai_strerror(err));
+  }
+
+  if ((lfd = socket(res->ai_family, res->ai_socktype,
+          res->ai_protocol)) < 0) {
+    die("Couldn't get listen socket.");
+  }
+
+  if ((err = bind(lfd, res->ai_addr, res->ai_addrlen))) {
+    die(gai_strerror(err));
+  }
+
+  if ((err = listen(lfd, MAX_CONNECTIONS))) {
+    die(gai_strerror(err));
+  }
+
+  freeaddrinfo(&res);
+  setnonblock(lfd);
+
+  return lfd;
 }
--- util.h
+++ util.h
@@ -3,8 +3,17 @@
 #include <stdio.h>
 #include <poll.h>
 
+/* Print error message and exit */
+void die(const char *strmsg);
+
 /* Send all bytes to a socket */
 int sendall(int sfd, char *buf, int len, int flags);
 
 /* Close a socket in a poll array. */
 void hangup(int *sfd);
+
+/* Set a socket to non-blocking mode for asynchronous I/O. */
+void setnonblock(int fd);
+
+/* Socket, bind, and listen routine */
+int getlistenfd(char *port);
