842819d29d46d27cd90eaa436b3a8a9f08f285e2
Kevin Berry <xckjb88@gmail.com>
2011-05-21 20:10:18 -0400

    bugs

--- echoserver-mt-poll.c
+++ echoserver-mt-poll.c
@@ -36,40 +36,50 @@ int main(int argc, char **argv) {
   pthread_t threads[NUM_THREADS];
   int thread_args[NUM_THREADS];
   int num_current_conns[NUM_THREADS];
+  struct pollfd listenpollfd[1];
 
   listenfd = getlistenfd(argv[1]);
+  listenpollfd[0].fd = listenfd;
+  listenpollfd[0].events = POLLIN;
+  listenpollfd[0].revents = 0;
 
   /* initialize poll fds */
   for (i = 0; i < NUM_THREADS; i++) {
     for (x = 0; x < MAX_CONNECTIONS; x++) {
       pollfds[i][x].fd = -1;
+      pollfds[i][x].events = POLLIN | POLLHUP;
+      pollfds[i][x].revents = 0;
     }
   }
 
   /* Start fixed amount of listening threads */
   for (i = 0; i < NUM_THREADS; i++) {
     thread_args[i] = i;
-    thread_create(&threads[i], NULL, thread_main, &thread_args[i]);
+    pthread_create(&threads[i], NULL, thread_main, &thread_args[i]);
   }
 
   /* accept loop. Delegate accept'ed fds to a thread */
   for (;;)  {
-    int sfd = accept(listenfd, (struct sockaddr*)&their_addr,
-        &addr_size);
+    if (poll(listenpollfd, 0, 0)) {
+      if (listenpollfd[0].revents & POLLIN) {
+        int sfd = accept(listenfd, (struct sockaddr*)&their_addr,
+            &addr_size);
 
-    /* add fd to thread with least amount of connections */
-    for(i = 0; i < NUM_THREADS; i++)  {
-      if (num_current_conns[i] < lowest_conns) {
-        tid = i;
-        lowest_conns = num_current_conns[i];
+        /* add fd to thread with least amount of connections */
+        for(i = 0; i < NUM_THREADS; i++)  {
+          if (num_current_conns[i] < lowest_conns) {
+            tid = i;
+            lowest_conns = num_current_conns[i];
+          }
+        }
+        /* Find first open fd slot for our thread */
+        for (i = 0; i < MAX_CONNECTIONS; i++) {
+          if (i == -1)  { break;  }
+        }
+        setnonblock(sfd);
+        pollfds[tid][i].fd = sfd;
       }
     }
-    /* Find first open fd slot for our thread */
-    for (i = 0; i < MAX_CONNECTIONS; i++) {
-      if (i == -1)  { break;  }
-    }
-    setnonblock(sfd);
-    pollfds[tid][i].fd = sfd;
   }
 
   /* Wait for threads to complete. */
@@ -88,8 +98,8 @@ void *thread_main (void *ptid)  {
   while(1)  {
     if (poll(pollfds[tid], MAX_CONNECTIONS, 100))  {
       for (i = 0; i < MAX_CONNECTIONS; i++) {
-        if (pollfds[tid].revents & POLLHUP) {
-          hangup(&pollfds[tid][i]);
+        if (pollfds[tid][i].revents & POLLHUP) {
+          hangup(&pollfds[tid][i].fd);
         } else if (pollfds[tid][i].revents & POLLIN) {
           /* echo data */
         }
