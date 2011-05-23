d9d94acedbee8ce35bb927c02a4866b95485e704
Kevin Berry <xckjb88@gmail.com>
2011-05-23 02:01:46 -0400

    actually make poll version non-blocking.

--- echoserver-poll.c
+++ echoserver-poll.c
@@ -15,11 +15,6 @@
 #define MAX_CONNECTIONS 20
 #define BUFLEN 1024
 
-void die(const char *strerr)  {
-  fprintf(stderr, "%s\n", strerr);
-  exit(errno);
-}
-
 int main(int argc, char **argv) {
 
   struct addrinfo hints, *res;
@@ -62,12 +57,7 @@ int main(int argc, char **argv) {
       sizeof(int)))))
     die(gai_strerror(err));
 
-  /* set sockets to be non blocking */
-  for (i = 0; i < MAX_CONNECTIONS; i++) {
-    sflags = fcntl(pollfds[i].fd, F_GETFL);
-    sflags |= O_NONBLOCK;
-    fcntl(pollfds[i].fd, F_SETFL, sflags);
-  }
+  setnonblock(pollfds[0].fd);
 
   /* set the first poll socket to the listen socket. */
   pollfds[0].fd = listenfd;
@@ -84,6 +74,7 @@ int main(int argc, char **argv) {
              * slot in our list of pollable sockets */
             newfd = accept(pollfds[0].fd, (struct sockaddr*)&their_addr,
                 &addr_size);
+            setnonblock(newfd);
             for (x = 1; x < MAX_CONNECTIONS; x++)  {
               if (pollfds[x].fd == -1) {
                 pollfds[x].fd = newfd;
@@ -103,9 +94,8 @@ int main(int argc, char **argv) {
             else if (pollfds[i].revents & POLLIN) {
               int numbytes = 0;
               char buf[BUFLEN];
-              while ((numbytes = recv(pollfds[i].fd, buf, sizeof(buf), 0)) > 0) {
-                sendall(pollfds[i].fd, buf, numbytes, 0);
-              }
+              numbytes = recv(pollfds[i].fd, buf, sizeof(buf), 0);
+              send(pollfds[i].fd, buf, numbytes, 0);
               /* zero bytes recv'd on a POLLIN event means that the remote
                * connection was probably closed. Hang up in this situation. */
               if (numbytes == 0)  {
